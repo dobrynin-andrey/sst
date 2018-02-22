f7042c7c5d9737fa266f390336f2a3c9ba4ea14c
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-02-23 00:06:37 +0700

    Git init

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Self-Service Technology',
+	'description' => 'Self-Service Technology',
+	'keywords' => 'Self-Service Technology',
+	'robots' => 'index, follow'
+);
+?>
