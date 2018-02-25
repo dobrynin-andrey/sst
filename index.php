5bb642331aaad86f0e12909de57db35e06b741db
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-02-25 19:36:26 +0700

    Added chenges to html template

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 // Вся верстка в header.php
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle("Self-Service Technology (SST)");
+$APPLICATION->SetTitle("SST - Self-Service Technology");
 $APPLICATION->SetPageProperty("description","Self-Service Technology (SST) – российский разработчик специализированной техники для розничных сетей любых масштабов. Основная продукция – программно - аппаратный кассовый комплекс (SCO), работающий по технологии самообслуживания.");
 ?>
